e8aaf278cd0e3d4d007b147bcb4f4fc73e217153
MMcClure11 <mcclme02@luther.edu>
2021-03-04 14:48:15 -0600

    Use TDD for subtract

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,8 +1,15 @@
 require './calculator.rb'
 
-describe "calculator" do 
-  it "adds numbers" do
-    calc = Calculator.new
-    expect(calc.add(2,3)).to eql(5)
+describe Calculator do 
+  context "Given two numbers" do 
+    it "can add the numbers using the add method" do
+      calc = Calculator.new
+      expect(calc.add(2,3)).to eql(5)
+    end
+
+    it "can subtract the numbers using the subtract method" do 
+      calc = Calculator.new
+      expect(calc.subtract(6,4)).to eql(2)
+    end
   end
 end
